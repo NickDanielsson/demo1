5e3e47b240f44484c20f8a77a74b523882e96ba0
Nick Danielsson <nickdanielsson87@gmail.com>
2021-04-13 15:15:49 +0200

    Final string, Query

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,14 +2,17 @@ package com.example.demo;
 
 import com.example.demo.models.Category;
 import com.example.demo.models.Kompis;
+import com.example.demo.models.Product;
 import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.repositories.KompisRepository;
+import com.example.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.Query;
 
 @SpringBootApplication
 public class Demo1Application {
@@ -18,6 +21,8 @@ public class Demo1Application {
     private KompisRepository kompisRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
@@ -27,9 +32,13 @@ public class Demo1Application {
     @Bean
     InitializingBean sendDatabase() {
         return () -> {
-            categoryRepository.save(new Category( "Mejeri"));
-
+           final String x = "INSERT INTO product (title, description, image, price, category_id) VALUES ('Tomater','Runda och fina tomater. Skölj och ha i salladen eller på smörgåsen. Observera att ursprungsland för denna produkt kan variera beroende på säsong och tillgång.','/images/fruit/tomatoes-1238255_640.jpg',39.9,1)";
+       // @Query(x)
+            //      productRepository.save(new Product("Runda och fina tomater. Skölj och ha i salladen eller på smörgåsen. Observera att ursprungsland för denna produkt kan variera beroende på säsong och tillgång.", "/images/fruit/tomatoes-1238255_640.jpg",39.90,1))
+          //  categoryRepository.save(new Category( "Mejeri"));
+        //    @Query(value = "1,'Tomater','Runda och fina tomater. Skölj och ha i salladen eller på smörgåsen. Observera att ursprungsland för denna produkt kan variera beroende på säsong och tillgång.','/images/fruit/tomatoes-1238255_640.jpg',39.9,1")
         };
+
     }
 
 
