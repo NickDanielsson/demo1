29772ec8b6d95ffb4412c63518a92072f93a6742
Nick Danielsson <nickdanielsson87@gmail.com>
2021-04-14 14:21:01 +0200

    added @CrossOrigin in Controller

--- src/main/java/com/example/demo/controllers/KompisController.java
+++ src/main/java/com/example/demo/controllers/KompisController.java
@@ -6,13 +6,10 @@ import com.example.demo.repositories.KompisRepository;
 import com.example.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
+// @RequestMapping(path = "/
 @Controller
-// @RequestMapping(path = "/demo")
+@CrossOrigin
 public class KompisController {
 
     @Autowired
