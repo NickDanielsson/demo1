0fcc823d123c9bd226c6fc07fbe524c1149a4a5d
Nick Danielsson <nickdanielsson87@gmail.com>
2021-04-14 14:01:46 +0200

    test

--- src/main/java/com/example/demo/controllers/KompisController.java
+++ src/main/java/com/example/demo/controllers/KompisController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("https://grupp5hakimlivs.herokuapp.com")
+// @RequestMapping(path = "/demo")
 public class KompisController {
 
     @Autowired
